390e0c37607dd1758dea30fe0b842a9d59ddfed5
Zhao <zhouqizhaozijian@gmail.com>
2018-05-21 14:40:29 +0800

    Restart: Add three implementations for REINFORCE

--- REINFORCE/custom_reinforce.py
+++ REINFORCE/custom_reinforce.py
@@ -42,7 +42,7 @@ class Policy(nn.Module):
     def forward(self, x):
         x = F.relu(self.lin1(x))
         action_scores = self.lin2(x)
-        return F.softmax(action_scores)
+        return F.softmax(action_scores, dim=-1)
 
 policy = Policy()
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
@@ -50,10 +50,67 @@ optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 def select_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
     probs = policy(Variable(state))
-    action = probs.multinomial()
+    action = probs.multinomial(1).item()
     policy.saved_actions.append(action)
     policy.log_probs.append(torch.log(probs))
-    return action.data
+    return action
+
+def grad_cal_method1(rewards, actions, log_probs):
+    """
+    Procedure:
+        * for each unique probability distribution vector that sample at one step;
+        * construct a grad vector which has minus reward in the sampled index;
+        * backward for list of the prob vectors
+    """
+    logs = []
+    grads = []
+    for action, log_prob, r in zip(actions, log_probs, rewards):
+        grad = torch.zeros(log_prob.size())
+        grad[0, action] = -r
+        logs.append(log_prob)
+        grads.append(grad)
+    optimizer.zero_grad()
+    autograd.backward(logs, grads)
+    optimizer.step()
+
+def grad_cal_method2(rewards, actions, log_probs):
+    """
+    Procedure:
+        * for each unique probability distribution vector that sample at one step;
+        * get the sampled unit from prob vector and concat them into a big tensor; 
+        * construct the rewards into a big vector which has same shape as the tensor above;
+        * backward for the grouped vector
+    """
+    logs = []
+    for action, log_prob in zip(actions, log_probs):
+        logs.append(log_prob[:, action])
+    logs = torch.cat(logs, dim=0)
+    optimizer.zero_grad()
+    logs.backward(-rewards)
+    optimizer.step()
+
+def grad_cal_method3(rewards, actions, log_probs):
+    """
+    Assumtion: backward function will backprop 1 if no values are provided.
+
+    Method: construct a loss that can backprop correct gradients using chain rules.
+
+    Procedure:
+        * for each unique probability distribution vector that sample at one step;
+        * get the sampled unit from prob vector and concat them into a big tensor; 
+        * construct the rewards into a big vector which has same shape as the tensor above;
+        * group the two tensors above into a formula using operations such as times or addition;
+        * the formula finally output a scalar, which get gradient 1 and backprop.
+    """
+    logs = []
+    for action, log_prob in zip(actions, log_probs):
+        logs.append(log_prob[:, action])
+    logs = torch.cat(logs, dim=0)
+    loss = - logs * rewards
+    loss_sum = torch.sum(loss)
+    optimizer.zero_grad()
+    loss_sum.backward()
+    optimizer.step()
 
 def finish_episode():
     R = 0
@@ -63,17 +120,11 @@ def finish_episode():
         rewards.insert(0, R)
     rewards = torch.Tensor(rewards)
     # numeric error avoid 
-    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
-    logs = []
-    grads = []
-    for action, log_prob, r in zip(policy.saved_actions, policy.log_probs, rewards):
-        grad = torch.zeros(log_prob.size())
-        grad[0, action.data[0,0]] = -r
-        logs.append(log_prob)
-        grads.append(grad)
-    optimizer.zero_grad()
-    autograd.backward(logs, grads)
-    optimizer.step()
+    eps = float(np.finfo(np.float32).eps)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+
+    grad_cal_method3(rewards, policy.saved_actions, policy.log_probs)
+
     del policy.rewards[:]
     del policy.saved_actions[:]
     del policy.log_probs[:]
@@ -83,7 +134,7 @@ for i_episode in count(1):
     state = env.reset()
     for t in range(10000):
         action = select_action(state)
-        state, reward, done, _ = env.step(action[0,0])
+        state, reward, done, _ = env.step(action)
         if args.render:
             env.render()
         policy.rewards.append(reward)
